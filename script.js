2505b30c92989a7a202b37d93b780253b011a5bb
Ayush1M <worklife2442@gmail.com>
2022-11-07 22:58:48 +0530

    given each conversion value its own variable to make it clear.

--- script.js
+++ script.js
@@ -2,18 +2,21 @@ const inputNumber = document.getElementById("input-number")
 const mFt = document.getElementById("mft")
 const lGa = document.getElementById("lga")
 const kGp = document.getElementById("kgp")
+const METER_TO_FEET = 3.28084
+const LITER_TO_GALLON = 0.264172
+const KILO_TO_POUND = 2.20462
 
 
 document.getElementById("convert-btn").addEventListener("click", () =>{
 
   const number = parseInt(inputNumber.value)
 
-  const meter = number*3.28084
-  const feet = number/3.28084
-  const liter = number*0.264172
-  const gallon = number/0.264172
-  const kilo = number*2.20462
-  const pound = number/2.20462
+  const meter = number*METER_TO_FEET
+  const feet = number/METER_TO_FEET
+  const liter = number*LITER_TO_GALLON
+  const gallon = number/LITER_TO_GALLON
+  const kilo = number*KILO_TO_POUND
+  const pound = number/KILO_TO_POUND
 
   mFt.textContent = `${number} meters = ${meter.toFixed(3)} feet | ${number} feet = ${feet.toFixed(3)} meters`
   lGa.textContent = `${number} liters = ${liter.toFixed(3)} gallons | ${number} gallons = ${gallon.toFixed(3)} liters`
